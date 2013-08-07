e6dadc5f42e7e141180ce80df75a626a5cc359f2
Joe Zhang <Joe.Zhang@EF.com>
2013-08-07 13:19:39 +0800

    add the project

---
+++ TuningCars/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TuningCars
+//
+//  Created by zhangkai on 8/7/13.
+//  Copyright (c) 2013 zhangkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
